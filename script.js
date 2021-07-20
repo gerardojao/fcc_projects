24904b23cb931478d3ef98d6740bfa80ddd021a3
gerardojao <gerardojao@gmail.com>
2021-07-20 18:02:31 -0430

    CONVERT TO ROMAN

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ let palindrome = (str)=>{
 }
 
 console.log(palindrome("casac"));
+
+//ROMAN NUMERAL CONVERTER
+
+function convertToRoman(num) {
+    let roman = {
+        
+        M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1 
+    },
+  romanText ="";
+  
+    for(let item in roman){
+        while(num >= roman[item]){
+        romanText+=item
+        num-=roman[item]
+       }
+      }
+  return romanText;
+  }
+  
+  console.log(convertToRoman(4999))
